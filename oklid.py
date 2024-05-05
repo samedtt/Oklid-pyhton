8654bee614f9f796adab97d729385884ac32e945
samedtt <158656857+samedtt@users.noreply.github.com>
2024-05-05 19:55:54 +0300

    Update oklid.py

--- oklid.py
+++ oklid.py
@@ -3,10 +3,8 @@ import math
 def eudistance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-# Örnek noktalar
 x1, y1 = 3,5
 x2, y2 = 2,1
 
-# Öklid mesafesini hesapla
 distance = eudistance(x1, y1, x2, y2)
 print("Öklid Mesafesi:", distance)
